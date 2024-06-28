c24571cfb9abf3adb1732cc7b8d0886f531d3ef0
rileygoerg <rgoerg@usf.edu>
2024-06-28 11:39:14 -0400

    Implement error handling for data fetching.

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ function call(){
         document.getElementById("itemPic").src = `${data[count].image}`
         document.getElementById("itemInfo").textContent = `${data[count].description}`
         document.getElementById("itemPrice").textContent = `${data[count].price}`
+    }).catch((error)=>{
+        console.log("There was an error loading the data. Make sure you have a server set up to fetch the API.")
+    }).finally(()=>{
+        console.log("Request loaded successfully.")
     })
 }
 call()
